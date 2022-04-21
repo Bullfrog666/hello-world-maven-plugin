c3537451c6817bbdec5ad7e6c0d28c12fb938ae6
Bullfrog <syczjason@gmail.com>
2022-04-21 15:50:19 +0200

    update gitignore + log info version groupId artifatcId

--- src/main/java/com/test/HelloWorldMojo.java
+++ src/main/java/com/test/HelloWorldMojo.java
@@ -12,7 +12,14 @@ public final class HelloWorldMojo extends AbstractMojo {
     @Parameter(required = true)
     public String name;
 
+    Package mainPackage = HelloWorldMojo.class.getPackage();
+    String version = mainPackage.getImplementationVersion();
+    String groupId = mainPackage.getName();
+    String artifactId = mainPackage.getImplementationTitle();
+
+
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("Hello " + name);
+        getLog().info("Version : " + version + "\nGroupId : " + groupId + "\nArtifactId : " + artifactId);
     }
 }
